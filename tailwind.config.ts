2a2605c54bccc5600a88bac28cc49c8987fd07ad
Jerommariyadaniyal <daniyal@Jerommariyadaniyals-MacBook-Air.local>
2026-08-31 13:08:29 +0530

    Initial commit from Create Next App

---
+++ tailwind.config.ts
@@ -0,0 +1,32 @@
+import type { Config } from "tailwindcss";
+
+const config: Config = {
+  content: [
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        background: "var(--background)",
+        foreground: "var(--foreground)",
+        slate: {
+          850: '#151e2e',
+          900: '#0f172a',
+          950: '#020617',
+        }
+      },
+      fontFamily: {
+        sans: ['var(--font-inter)'],
+        display: ['var(--font-outfit)'],
+      },
+      backgroundImage: {
+        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
+        'hero-gradient': 'linear-gradient(to bottom, rgba(2,6,23,0.3) 0%, rgba(2,6,23,1) 100%)',
+      }
+    },
+  },
+  plugins: [],
+};
+export default config;
